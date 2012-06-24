1fe7e8b5e5f47c2b50641bbc6afcd7cecfc31103
Glen Hunt <glenrhunt@gmail.com>
2012-06-23 22:41:33 -0700

    setting up collections

--- app/Controller/CollectionsController.php
+++ app/Controller/CollectionsController.php
@@ -10,5 +10,11 @@ class CollectionsController extends AppController {
                                                        array('Collection.title')));
     $this->set('collections', $collections);
   }
+
+  public function add() {
+    // TODO: the whole if(post) thing, add it, make sure to use the
+    // proper alert classes and such. additionally, make sure to
+    // validate in the model.
+  }
 }
 ?>
