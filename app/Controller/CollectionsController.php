c9e8a3735f1913224404fcfe24ad4f0c6820faea
Glen Hunt <glenrhunt@gmail.com>
2012-06-23 22:37:15 -0700

    setting up models

---
+++ app/Controller/CollectionsController.php
@@ -0,0 +1,14 @@
+<?
+class CollectionsController extends AppController {
+  public $name = 'Collection';
+  
+  public function beforeFilter() {
+  }
+
+  public function manage() {
+    $collections = $this->Collection->find('all', array('order'=>
+                                                       array('Collection.title')));
+    $this-set('collections', $collections);
+  }
+}
+?>
