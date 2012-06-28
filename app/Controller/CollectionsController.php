b3da6e644dd2410afa9956d28cf55d5bb3d6291b
Glen Hunt <glenrhunt@gmail.com>
2012-06-27 21:37:59 -0700

    collection management

--- app/Controller/CollectionsController.php
+++ app/Controller/CollectionsController.php
@@ -57,9 +57,7 @@ class CollectionsController extends AppController {
       $this->redirect(array('controller'=>'collections', 'action'=>'manage'));
     
     $this->Collection->id = $id;
-    $collection = $this->Collection->find('threaded',
-                                          array('conditions'=>
-                                                array('Collection.id'=>$id)));
+    $collection = $this->Collection->findById($id);
     $this->set('collection', $collection);
   }
 }
