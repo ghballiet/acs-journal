1310d46c224797e712ab59441a122c3057d9aee5
Glen Hunt <glenrhunt@gmail.com>
2012-07-22 14:26:17 -0700

    updating table of contents

--- app/Model/Collection.php
+++ app/Model/Collection.php
@@ -21,6 +21,10 @@ class Collection extends AppModel {
     $title = $this->data['Collection']['title'];
     $title = strtolower(trim($title));
     $slug = preg_replace('/\W+/', '', $title);
+    
+    pr($slug);
+    die('');
+
     $slug = str_replace(' ', '-', $slug);
     $slug = sprintf('%d-%s', $id, $title);
     $this->data['Collection']['slug'] = $slug;
