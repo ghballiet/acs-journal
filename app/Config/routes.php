21820f655f72fe1cfd6debeec7b5cc384eaf0dd6
Glen Hunt <glenrhunt@gmail.com>
2012-08-06 23:27:44 -0700

    added old conf link

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -29,6 +29,7 @@ Router::parseExtensions('pdf');
 
 
 Router::connect('/', array('controller'=>'pages', 'action'=>'display', '2012'));
+Router::connect('/acs/2012', array('controller'=>'pages', 'action'=>'display', '2012'));
 Router::connect('/login', array('controller'=>'users', 'action'=>'login'));
 Router::connect('/logout', array('controller'=>'users', 'action'=>'logout'));
 Router::connect('/register', array('controller'=>'users', 'action'=>'register'));
