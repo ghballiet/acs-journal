b290f693373f785b8310e9973b3e535faece10ea
Glen Hunt <glenrhunt@gmail.com>
2012-08-13 22:22:07 -0700

    updates to routing

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -30,10 +30,10 @@ Router::parseExtensions('pdf');
 
 Router::connect('/', array('controller'=>'pages', 'action'=>'display', '2012'));
 Router::connect('/acs/2012', array('controller'=>'pages', 'action'=>'display', '2012'));
-Router::connect('/acs/2011', '/acs-old/');
+// Router::connect('/acs/2011', '/acs-old/');
 Router::connect('/login', array('controller'=>'users', 'action'=>'login'));
 Router::connect('/logout', array('controller'=>'users', 'action'=>'logout'));
-// Router::connect('/register', array('controller'=>'users', 'action'=>'register'));
+Router::connect('/register', array('controller'=>'users', 'action'=>'register'));
 Router::connect('/dashboard', array('controller'=>'users', 'action'=>'dashboard'));
 Router::connect('/submit', array('controller'=>'submissions', 'action'=>'create'));
 Router::connect('/journal/submit', array('controller'=>'submissions', 'action'=>'create', 'volume-2'));
