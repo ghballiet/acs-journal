b1acdfd7ac9cedf96d53479dc6761eadc7454947
Glen Hunt <glenrhunt@gmail.com>
2012-08-19 21:39:42 -0700

    changed so gravatar will use identicon

--- app/View/Helper/ProfileHelper.php
+++ app/View/Helper/ProfileHelper.php
@@ -5,7 +5,7 @@ class ProfileHelper extends AppHelper {
   public $helpers = array('Html');
 
   public function gravatar($email, $size = '40px') {
-    $base_url = 'http://www.gravatar.com/avatar/%s?d=mm&s=%s&r=g';
+    $base_url = 'http://www.gravatar.com/avatar/%s?d=identicon&s=%s&r=g';
     $hash = md5(strtolower(trim($email)));
     $url = sprintf($base_url, $hash, $size);
     return $this->Html->image($url, array('class'=>'gravatar'));
